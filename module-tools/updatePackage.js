d0ace7d182af9c2ab937cbbddc24cd49f41e4100
Mert Cingöz <mertcingoz@gmail.com>
2023-10-13 18:44:11 +0200

    RAD-4929 Update organization

--- module-tools/updatePackage.js
+++ module-tools/updatePackage.js
@@ -1,18 +1,5 @@
-/**
- * Copyright 2020 Infinite Automation Systems Inc.
- * http://infiniteautomation.com/
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+/*
+ * Copyright (C) 2023 Radix IoT LLC. All rights reserved.
  */
 
 const path = require('path');
@@ -26,12 +13,12 @@ module.exports = function updatePackage(pom, directory = path.resolve('.'), writ
     return readPackage(directory).then(packageJson => {
         const originalPackageJson = _.cloneDeep(packageJson);
 
-        if (packageJson.com_infiniteautomation == null) {
-            packageJson.com_infiniteautomation = {};
+        if (packageJson.com_radixiot == null) {
+            packageJson.com_radixiot = {};
         }
 
-        packageJson.com_infiniteautomation.moduleName = pom.project.name[0];
-        packageJson.name = '@infinite-automation/' + _.kebabCase(pom.project.name[0]);
+        packageJson.com_radixiot.moduleName = pom.project.name[0];
+        packageJson.name = '@radixiot/' + _.kebabCase(pom.project.name[0]);
         // version tag may not be present or may contain a property
         if (pom.project.version && !pom.project.version[0].includes('${')) {
             packageJson.version = pom.project.version[0];
--- test-helper/testHelper.js
+++ test-helper/testHelper.js
@@ -1,25 +1,12 @@
-/**
- * Copyright 2020 Infinite Automation Systems Inc.
- * http://infiniteautomation.com/
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
+/*
+ * Copyright (C) 2023 Radix IoT LLC. All rights reserved.
  */
 
 const Mocha = require('mocha');
 const path = require('path');
 const chai = require('chai');
-const MangoClient = require('@infinite-automation/mango-client');
-const defer = require('@infinite-automation/mango-client/src/util').defer;
+const MangoClient = require('@radixiot/mango-client');
+const defer = require('@radixiot/mango-client/src/util').defer;
 const uuid = require('uuid/v4');
 
 // Monkey patch the Mocha Runner to add the response data into the Error message
